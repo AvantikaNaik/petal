467cf7e2cce4d12c2577a31c8231d79fdc7912fd
Avantika Naik <tikanaik@gmail.com>
2021-08-07 16:15:21 -0400

    fixed login required

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -9,17 +9,15 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-def login_required(f):
-    """
-    Decorate routes to require login.
-    http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
-    """
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None:
-            return redirect("/login")
-        return f(*args, **kwargs)
-    return decorated_function
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view 
 
 @bp.before_app_request
 def load_logged_in_user():
@@ -106,4 +104,4 @@ def event():
     if request.method == "POST":
         event = request.form.get("event")
     else: 
-        return render_template("event.html")
+        return render_template("auth/event.html")
